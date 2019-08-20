fbfa834dba1ee6fe699b766e3955bf0b2bc93e9a
Sheldon Reddy <sheldonreddy@gmail.com>
2019-08-20 12:05:16 +0200

    P_NEW_TelegramMessengerClient_Complete

---
+++ TelegramMessengerClient/TelegramMessengerClient/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TelegramMessengerClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.Authenticate = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ImageSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(32, 23);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(130, 21);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Authenticate
+            // 
+            this.Authenticate.Location = new System.Drawing.Point(32, 63);
+            this.Authenticate.Name = "Authenticate";
+            this.Authenticate.Size = new System.Drawing.Size(130, 21);
+            this.Authenticate.TabIndex = 1;
+            this.Authenticate.Text = "Authenticate";
+            this.Authenticate.UseVisualStyleBackColor = true;
+            this.Authenticate.Click += new System.EventHandler(this.Authenticate_Click);
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(32, 107);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(130, 21);
+            this.message.TabIndex = 2;
+            this.message.Text = "Message Contact";
+            this.message.UseVisualStyleBackColor = true;
+            this.message.Click += new System.EventHandler(this.message_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 21);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Message Channel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(212, 166);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(514, 56);
+            this.listBox1.TabIndex = 4;
+            // 
+            // ImageSend
+            // 
+            this.ImageSend.Location = new System.Drawing.Point(32, 201);
+            this.ImageSend.Name = "ImageSend";
+            this.ImageSend.Size = new System.Drawing.Size(130, 21);
+            this.ImageSend.TabIndex = 5;
+            this.ImageSend.Text = "Send Imge";
+            this.ImageSend.UseVisualStyleBackColor = true;
+            this.ImageSend.Click += new System.EventHandler(this.ImageSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Instructions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Click Connect";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Click Message Contact/ Message Channel or Send Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(316, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "If Exception is Caught, you must Authenticate - Click Authenticate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 238);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImageSend);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.Authenticate);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Authenticate;
+        private System.Windows.Forms.Button message;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button ImageSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TelegramMessengerClient/TelegramMessengerClient/Form1.cs
@@ -0,0 +1,222 @@
+﻿/*
+ * Project:         Telegram Messenger Client Application
+ * Description:     This application is used to test the TLSharp Library
+ *                  It sends text messages to a Telegram and a Telegram Channel.
+ *                  It also sends an image to a Telegram User
+ * 
+ * Author:  Sheldon Reddy
+ * Date:    20190820
+ * 
+ * Dependencies: TLSharp [ Nuget> Install-Package TLSharp ]
+ * 
+ * Resources
+ *  https://github.com/sochix/TLSharp [Shows how to perform functions not in the list as well]
+ *  https://github.com/sochix/TLSharp/blob/master/TLSharp.Tests/TLSharpTests.cs#L107
+ *  https://my.telegram.org/apps
+ * 
+ * IMPORTANT    - Add your custom apiId and apiHash to run the program - This is found in the Globals Region
+ *              - Before Proceeding, read each message to find the instructions - pretty simple
+ *              - You only need to AUTH the User once. 
+ *      
+ * STATUS: COMPLETED           
+ *              
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Telegram
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+using TLSharp.Core.Network;
+using TLSharp.Core.Requests;
+using TLSharp.Core.Utils;
+
+namespace TelegramMessengerClient
+{
+    public partial class Form1 : Form
+    {
+
+        #region Globals
+        private TelegramClient client;
+        // Change these values to your own custom values obtained from https://my.telegram.org/apps
+        private int apiId = 0;
+        private string apiHash = "";
+        private string destNumber = "27829410963";
+        private string sourceNumber = "+27739993081";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        /// <summary>
+        ///     CONNECT_CLICK Connect the Telegram Client to the Telegram Service to 
+        ///     allow the C# application to perform typical Messaging Functions.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TelegramClient(apiId, apiHash);
+                await client.ConnectAsync();
+                listBox1.Items.Add("Connected");
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add("Exception: "+ex.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        ///     AUTHENTICATE_CLICK is responsible for Authenticating the User ( using Cell Number )
+        ///     to allow the C# Application to perform all Messaging functions ( Only Authenticated users can perform all functions)
+        ///     See https://core.telegram.org/api/auth to see what Function Non-Auth Users can perform.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Authenticate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var hash = await client.SendCodeRequestAsync(sourceNumber);
+
+                // Check your phone for the Code Sent by Telegram, 
+                // Enter it below, Remove the debug breakpoint and re-run
+                var code = "48097";
+
+                var user = await client.MakeAuthAsync(sourceNumber, hash, code);
+
+                if(user!=null)
+                {
+                    listBox1.Items.Add("User Authenticated for: "+ sourceNumber);
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Exception: " + ex.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        ///     MESSAGE_CLICK sends a text message to a Telegram User found in the contact list of the Authenticated User
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void message_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get available contacts
+                var result = await client.GetContactsAsync();
+
+                //find recipient in contacts
+                var user = result.Users
+                    .Where(x => x.GetType() == typeof(TLUser))
+                    .Cast<TLUser>()
+                    .FirstOrDefault(x => x.Phone == destNumber);
+
+                if (user == null)
+                {
+                    throw new System.Exception("Number was not found in Contacts List of user: " + sourceNumber);
+                }
+
+                //send message
+                await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, "Hello World!");
+                listBox1.Items.Add("Message Sent to: " + destNumber);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Exception: " + ex.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        ///     BUTTON4_CLICK Sends a text message to a Telegram Channel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var ret = await client.GetUserDialogsAsync();
+
+                // NB. The following TLDIAGLOSLICE Casting doesn't work so I manually browsed the ret for the chat.ID and AccessHash values
+                //get user dialogs
+                //var dialogs = (TLDialogsSlice)await client.GetUserDialogsAsync();
+
+                //find channel by title
+                //var chat = dialogs.Chats
+                //  .Where(c => c.GetType() == typeof(TLChannel))
+                //  .Cast<TLChannel>()
+                //  .FirstOrDefault(c => c.Title == "DIGNOSTM");
+
+                // Send message
+                // BEFORE PROCEEDING - Add a watch to "ret" above and manually inspect the chat.Id and AccessHash fields for your desired Channel and add it below.
+                // Then remove breakpoint and re-run.
+                await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = 00000/*chat.Id*/, AccessHash = 000000 /*chat.AccessHash.Value*/ }, "Hello World!");
+                listBox1.Items.Add("Message Sent to: " + "XXXXX Channel");
+                
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Exception: " + ex.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        ///     IMAGESEND_CLICK Sends a JPG Image to a Telegram User
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ImageSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await client.GetContactsAsync();
+
+                var user = result.Users
+                    .OfType<TLUser>()
+                    .FirstOrDefault(x => x.Phone == destNumber);
+
+                // Enter Image Title and ImageLocation before proceeding
+                var fileResult = (TLInputFile)await client.UploadFile("Image Title", new StreamReader(@"FullImageLocation"));
+                await client.SendUploadedPhoto(new TLInputPeerUser() { UserId = user.Id }, fileResult, "DIGNOSTM");
+                listBox1.Items.Add("Image Sent to: " + destNumber);
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add("Exception: " + ex.ToString());
+            }
+            
+        }
+    }
+}
